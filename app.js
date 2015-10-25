ea6c93d58ea6d12dbe9b14a3196ce8f83b2f11c4
Drahman AbdulBasir <durmon.albasir@gmail.com>
2015-10-25 17:15:51 -0400

    rearrange structure. fix cors

--- app.js
+++ app.js
@@ -15,12 +15,15 @@ var multer = require("multer");
 
 var app = express();
 
+app.use(allowCors);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,7 +31,58 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(multer());
 
-app.use("*", function(req, res, next) {
+require("express").Router().all("/", homeRender);
+app.use('/buildstatus', router.buildStatus);
+app.use("/hostname", router.hostname);
+
+// catch 404 and forward to error handler
+app.use("*", catch404);
+
+// error handlers
+if (app.get('env') === 'development') {
+	// development error handler
+	// will print stacktrace
+	app.use(devErrHandler);
+
+} else {
+
+	// production error handler
+	// no stacktraces leaked to user
+	app.use(prodErrHandler);
+}
+
+module.exports = app;
+
+
+function homeRender (req, res) {
+	res.render("Welcome to here!").end();
+}
+
+function catch404 (req, res, next) {
+	console.log("caught 404. forward to err middleware");
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+}
+
+function devErrHandler (err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: err
+	});
+}
+
+function prodErrHandler (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+}
+
+function allowCors (req, res, next) {
+	console.log("allow cors");
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, ontent-Length, X-Requested-With");
@@ -38,46 +92,5 @@ app.use("*", function(req, res, next) {
 	}
 
 	next();
-});
-
-//var router = require("express").Router();
-require("express").Router().all("/", function(req, res) {
-	res.render("Welcome to here!").end();
-});
-
-app.use('/buildstatus', router.buildStatus);
-app.use("/hostname", router.hostname);
-
-// catch 404 and forward to error handler
-app.use("*", function(req, res, next) {
-	console.log("err middleware");
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-module.exports = app;
