d7ed5da298ea4201be5471dc9828b8fb78a7e3d8
abdurrahman <durmon.albasir@gmail.com>
2015-12-30 14:23:15 -0500

    properties to account for mongoose / mongo's lack of ability to detemrine if thing is updated

--- models/schema-hostname.js
+++ models/schema-hostname.js
@@ -5,10 +5,23 @@ var hostname = new Schema({
     name:       { type: Schema.Types.String, default: "anonymous" },
     address:    { type: String },
     //message:    { type: Schema.Types.String }
-    message:	{ type: Schema.Types.Mixed }
+    message:	{ type: Schema.Types.Mixed },
+    isUpdated:	{ type: Schema.Types.Boolean, default: true }
 
 }, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } });
 
 hostname.index({ name: 1, address: 1 }, { unique: true });
 
+//hostname.pre("save", saveIfModified);
+
 module.exports = hostname;
+
+function saveIfModified (next) {
+	// isModified doesn't work. timestamps considered mod?
+	//console.log("isModfied?", this.isUpdated());
+	if (!this.isModified()) {
+		return;
+	}
+
+	next();
+}
